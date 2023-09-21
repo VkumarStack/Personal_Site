80bd70677170490c4ff0a20b682e10a45e97f3e3
VkumarStack <vksogi@gmail.com>
2023-09-21 12:23:33 -0700

    Restructure layout and add more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { cloneElement, useState, useEffect, useRef } from 'react'
-import './App.css'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Links from './components/Links'
-import { SwitchTransition, CSSTransition, TransitionGroup } from 'react-transition-group'
+import GithubIcon from './assets/Icons/github-mark.svg'
+import LinkedInIcon from './assets/Icons/linkedin.svg'
+import GmailIcon from './assets/Icons/gmail.svg'
 
+function App() {
+  return (
+    <div>
+      <nav>
+        <div className="pages">
+          <button type="button">About</button>
+          <button type="button">Skills</button>
+          <button type="button">Projects</button>
+        </div>
+        <div className="socials">
+          <a href="https://www.github.com/vkumarstack"><img className={"svg"} width={40} height={40} src={GithubIcon} alt="Github Icon"/></a>
+          <a href="https://www.linkedin.com/in/vivek-kumar-5424a5222"><img className={"svg"} width={40} height={40} src={LinkedInIcon} alt="LinkedIn Icon" /></a>
+          <a href="mailto: vksogi@gmail.com"><img className={"svg"} width={40} height={40} src={GmailIcon} alt="LinkedIn Icon" /></a>
+        </div>
+      </nav>
+      <div className="content">
+        <p> Blah</p>
+        <h1> Blah</h1>
+        <h1> Blah </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App
+
+/* 
 const dynamicChildFactory = classNames => child => 
   cloneElement(child, {
     classNames
@@ -16,9 +40,7 @@ function App() {
   const transitionDirection = currentPage > previousPage ? 'R' : 'L'
   const classNames = "page" + transitionDirection
 
-  const pages = [Projects, Skills, Links];
-
-  const nodeRef = useRef(null);
+  const pages = [About, Projects, Skills, Links];
 
 
   const renderPage = () => {
@@ -28,21 +50,30 @@ function App() {
     );
   }
 
+
   return (
     <div>
-      <div className="Menu">
-        <button type='button' onClick={() => { 
-          setPreviousPage(currentPage)
-          setCurrentPage(0) 
-          }}> Projects </button>
-        <button type='button' onClick={() => { 
-          setPreviousPage(currentPage)
-          setCurrentPage(1)
-          }}> Skills </button>
-        <button type='button' onClick={() => {
-          setPreviousPage(currentPage)
-          setCurrentPage(2)
-        }}> Links </button>
+      <div className="menu">
+        <div className="logo-group">
+          <button type="button" className={currentPage == 0 ? 'focus' : ''} onClick={() => {
+            setPreviousPage(currentPage)
+            setCurrentPage(0)
+          }}> About </button>
+        </div>
+        <div className="menu-group">
+          <button type='button' className={currentPage == 1 ? 'focus' : ''} onClick={() => {
+            setPreviousPage(currentPage)
+            setCurrentPage(1)
+            }}> Projects </button>
+          <button type='button' className={currentPage == 2 ? 'focus' : ''} onClick={() => {
+            setPreviousPage(currentPage)
+            setCurrentPage(2)
+            }}> Skills </button>
+          <button type='button' className={currentPage == 3 ? 'focus' : ''} onClick={() => {
+            setPreviousPage(currentPage)
+            setCurrentPage(3)
+          }}> Links </button>
+        </div>
       </div>
       
       <TransitionGroup childFactory={dynamicChildFactory(classNames)}>
@@ -60,3 +91,4 @@ function App() {
 }
 
 export default App
+*/
