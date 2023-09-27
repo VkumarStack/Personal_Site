8050cdb36be7a7f2126764dd2daa7ffc67f4fd50
VkumarStack <vksogi@gmail.com>
2023-09-26 20:38:16 -0700

    Finish Projects component (still need to add one more project)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react'
 import GithubIcon from './assets/Icons/github-mark.svg'
 import LinkedInIcon from './assets/Icons/linkedin.svg'
 import GmailIcon from './assets/Icons/gmail.svg'
@@ -7,13 +8,25 @@ import Projects from './components/Projects'
 import './App.css'
 
 function App() {
+  const testRef = useRef(null);
+  const aboutElement = useRef(null);
+  const skillsElement = useRef(null);
+  const projectsElement = useRef(null);
+  
+  useEffect(() => {
+    aboutElement.current = document.getElementsByClassName('about')[0];
+    skillsElement.current = document.getElementsByClassName('skills')[0];
+    projectsElement.current = document.getElementsByClassName('projects')[0];
+    
+  }, []);
+
   return (
     <div>
       <nav>
         <div className="pages">
-          <button type="button">About</button>
-          <button type="button">Skills</button>
-          <button type="button">Projects</button>
+          <button type="button" onClick={() => aboutElement.current.scrollIntoView({behavior: 'smooth', block: 'end'})}>About</button>
+          <button type="button" onClick={() => skillsElement.current.scrollIntoView({behavior: 'smooth', block: 'center'})}>Skills</button>
+          <button type="button" onClick={() => projectsElement.current.scrollIntoView({behavior: 'smooth'})}>Projects</button>
         </div>
         <div className="socials">
           <a href="https://www.github.com/vkumarstack"><img className={"svg"} width={40} height={40} src={GithubIcon} alt="Github Icon"/></a>
@@ -22,7 +35,7 @@ function App() {
         </div>
       </nav>
       <div className="content">
-        <About/>
+        <About ref={testRef}/>
         <Skills/>
         <Projects/>
       </div>
@@ -31,68 +44,3 @@ function App() {
 }
 
 export default App
-
-/* 
-const dynamicChildFactory = classNames => child => 
-  cloneElement(child, {
-    classNames
-  });
-
-function App() {
-  const [currentPage, setCurrentPage] = useState(0)
-  const [previousPage, setPreviousPage] = useState(0)
-  const transitionDirection = currentPage > previousPage ? 'R' : 'L'
-  const classNames = "page" + transitionDirection
-
-  const pages = [About, Projects, Skills, Links];
-
-
-  const renderPage = () => {
-    const Page = pages[currentPage]
-    return (
-      <Page/>
-    );
-  }
-
-
-  return (
-    <div>
-      <div className="menu">
-        <div className="logo-group">
-          <button type="button" className={currentPage == 0 ? 'focus' : ''} onClick={() => {
-            setPreviousPage(currentPage)
-            setCurrentPage(0)
-          }}> About </button>
-        </div>
-        <div className="menu-group">
-          <button type='button' className={currentPage == 1 ? 'focus' : ''} onClick={() => {
-            setPreviousPage(currentPage)
-            setCurrentPage(1)
-            }}> Projects </button>
-          <button type='button' className={currentPage == 2 ? 'focus' : ''} onClick={() => {
-            setPreviousPage(currentPage)
-            setCurrentPage(2)
-            }}> Skills </button>
-          <button type='button' className={currentPage == 3 ? 'focus' : ''} onClick={() => {
-            setPreviousPage(currentPage)
-            setCurrentPage(3)
-          }}> Links </button>
-        </div>
-      </div>
-      
-      <TransitionGroup childFactory={dynamicChildFactory(classNames)}>
-        <CSSTransition 
-          classNames={classNames}
-          key={currentPage}
-          timeout={1000}>
-            <div className="page">
-              {renderPage()}
-            </div>
-        </CSSTransition>
-      </TransitionGroup>
-    </div>
-  )
-}
-
-export default App
-*/
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,49 +4,67 @@ import OCREncryptionScreenshot from '../assets/Images/OCR_Encryption_Screenshot.
 import AdminDashboardScreenshot from '../assets/Images/Admin_Dashboard_Screenshot.png'
 import WitherScreenshot from '../assets/Images/Wither_Screenshot.png'
 import DigitClassifierScreenshot from '../assets/Images/Digit_Classifier_Screenshot.png'
+import ProjectInformation from '../assets/Information/Project_Info.json'
 import './Projects.css'
 
 class Projects extends React.Component {
   constructor() {
     super();
     this.modalRef = React.createRef(null);
+    this.state = {description: null, page: null, github: null};
+  }
+
+  handleModal(e) {
+    const info = ProjectInformation[e.currentTarget.id];
+    this.setState({description: info.description, page: info["page-link"], github: info["github-link"] })
+    this.modalRef.current.classList.add("visible");
+  }
+
+  renderModal() {
+    return (
+      <div ref={this.modalRef} className="project-modal" onClick={(e) => e.currentTarget.classList.remove("visible")}>
+        <div className="modal-info" onClick={(e) => e.stopPropagation()}>
+          <p> {this.state.description} </p>
+          <div className="modal-buttons">
+            { this.state.page !== null && <a href={this.state.page} target="_blank" rel="noreferrer"> <button> Project Link </button> </a> }
+            <a href={this.state.github} target="_blank" rel="noreferrer"> <button> Github Link </button> </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   render() {
     return (
-      <div className="Projects">
-        <div ref={this.modalRef} className="project-modal" onClick={(e) => e.currentTarget.classList.remove("visible")}>
-          <button onClick={(e) => {
-            e.stopPropagation();
-          }}> Link </button>
-        </div>
+      <div className="projects">
+        {this.renderModal()}
         <h1 className="projects-label"> Projects </h1>
         <div className="projects-container">
-          <div className="project-wrapper" onClick={() => this.modalRef.current.classList.add("visible")}> 
+          <div className="project-wrapper" id="Pixel" onClick={this.handleModal.bind(this)}> 
             <img src={PixelScreenshot} alt="Screenshot of page of application 'Pixel'" />
             <div>
               <h1> Pixel </h1>
             </div>
           </div>
-          <div className="project-wrapper"> 
+          <div className="project-wrapper" id="OCR_Encryption" onClick={this.handleModal.bind(this)}> 
             <img src={OCREncryptionScreenshot} alt="Screenshot of page of application 'OCR_Encrpytion'" />
             <div>
               <h1> OCR Encryption </h1>
             </div>
           </div>
-          <div className="project-wrapper"> 
+          <div className="project-wrapper" id="Wither" onClick={this.handleModal.bind(this)}> 
             <img src={WitherScreenshot} alt="Screenshot of page of application of 'Wither'" />
             <div>
               <h1>Wither</h1>
             </div>
           </div>
-          <div className="project-wrapper"> 
+          <div className="project-wrapper" id="Admin_Dashboard" onClick={this.handleModal.bind(this)}> 
             <img src={AdminDashboardScreenshot} alt="Screenshot of page of application 'Admin Dashboard'" />
             <div>
               <h1>Admin Dashboard</h1>
             </div>
           </div>
-          <div className="project-wrapper"> 
+          <div className="project-wrapper" id="Digit_Classifier" onClick={this.handleModal.bind(this)}> 
             <img src={DigitClassifierScreenshot} alt="Screenshot of tab of application 'Digit Classifier'" />
             <div>
               <h1> Digit Classifier </h1>
